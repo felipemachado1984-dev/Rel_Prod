04e15fca4387c53c0fe22f689bf5bf1f0291abbf
felipemachado1984-dev <felipe.machado1984@gmail.com>
2026-08-24 22:38:36 -0300

    Add blocks and lines to OcrResult structure

--- lib/services/ocr_service.dart
+++ lib/services/ocr_service.dart
@@ -4,8 +4,23 @@ import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart
 class OcrResult {
   final List<String> allNumbers;
   final String rawText;
+  final List<OcrBlock> blocks;
 
-  OcrResult({required this.allNumbers, required this.rawText});
+  OcrResult({
+    required this.allNumbers,
+    required this.rawText,
+    required this.blocks,
+  });
+}
+
+class OcrBlock {
+  final List<OcrLine> lines;
+  OcrBlock({required this.lines});
+}
+
+class OcrLine {
+  final String text;
+  OcrLine({required this.text});
 }
 
 class OcrService {
@@ -18,18 +33,27 @@ class OcrService {
 
     final rawText = result.text;
     final allNumbers = <String>[];
+    final blocks = <OcrBlock>[];
 
     for (final block in result.blocks) {
+      final lines = <OcrLine>[];
       for (final line in block.lines) {
         final lineText = line.text.trim();
+        lines.add(OcrLine(text: lineText));
+
         final numbers = RegExp(r'\d+').allMatches(lineText);
         for (final match in numbers) {
           allNumbers.add(match.group(0)!);
         }
       }
+      blocks.add(OcrBlock(lines: lines));
     }
 
-    return OcrResult(allNumbers: allNumbers, rawText: rawText);
+    return OcrResult(
+      allNumbers: allNumbers,
+      rawText: rawText,
+      blocks: blocks,
+    );
   }
 
   void dispose() {
